7b3b5ec849619bfa5cecd11ca28bc6ae1b4df208
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-12-09 12:55:50 -0600

    started working on the routing prob but decided to accept Taylor's fixes

--- front-end/src/app/flights/flights.module.ts
+++ front-end/src/app/flights/flights.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import {AppRoutingModule} from './flights-routing.module';
+
+import * as components from './components';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    components.HomeComponent,
+    components.CheckoutComponent,
+    components.ProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    FormsModule
   ]
 })
 export class FlightsModule { }
