012b48053fbfc394c64a96e4162d931767de1bea
James Justinic <13036935+jjustinic@users.noreply.github.com>
2019-12-09 15:48:58 -0600

    Add durations of Itinerary

--- back-end/src/main/java/com/aa/improvinghackathon/api/FlightSearchHandler.java
+++ back-end/src/main/java/com/aa/improvinghackathon/api/FlightSearchHandler.java
@@ -1,9 +1,11 @@
 package com.aa.improvinghackathon.api;
 
+import com.aa.improvinghackathon.domain.Flight;
 import com.aa.improvinghackathon.domain.Itinerary;
 import com.aa.improvinghackathon.infrastructure.service.flights.FlightEngineClient;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,8 +20,19 @@ public class FlightSearchHandler {
     }
 
     public List<Itinerary> flightSearch(final FlightSearch flightSearch) {
-        return flightEngineClient.retrieveFlights(flightSearch.getDate(), flightSearch.getOrigin())
-                .stream()
+        List<Itinerary> itineraries = new ArrayList<>();
+
+        List<Flight> allFlights = flightEngineClient.retrieveFlights(flightSearch.getDate(), flightSearch.getOrigin());
+        allFlights.stream()
+                .map(flight -> flight.getDestination().getCode())
+                .filter(code -> !code.equals(flightSearch.getDestination()))
+                .distinct()
+                .forEach(code -> allFlights.addAll(flightEngineClient.retrieveFlights(flightSearch.getDate(), code)));
+
+        // TODO: BUILD A TREE
+        
+
+        return allFlights.stream()
                 .map(flight -> new Itinerary(Arrays.asList(flight)))
                 .collect(Collectors.toList());
     }
