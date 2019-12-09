542e362fd288c1f07039dce903c0c4fa86ffde1c
Zach Marbach <Zach.Marbach@improving.com>
2019-12-09 13:34:16 -0600

    fixed formBuilder error

--- front-end/src/app/flights/components/home/home.component.ts
+++ front-end/src/app/flights/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { FlightsService } from 'src/app/service';
 import { Flight } from 'src/app/model.ts';
@@ -9,31 +8,29 @@ import { Flight } from 'src/app/model.ts';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  //implements OnInit
-
-  // flights: Flight[];
-  // form: FormGroup;
-  //
-  // constructor(private fbuilder: FormBuilder,
-  //   private flightsService: FlightsService) { }
-  //
-  // ngOnInit() {
-  //   this.form = this.fbuilder.group({
-  //     origin: '',
-  //     destination: '',
-  //     date: ''
-  //   });
-  //
-  // }
-  //
-  // sendToAPI() {
-  //   let formObj = this.form.getRawValue();
-  //
-  //   let serializedForm = JSON.stringify(formObj);
-  //
-  //   this.flightsService.search(serializedForm)
-  //     .then(flights => this.flights = flights);
-  // }
+  flights: Flight[];
+  form: FormGroup;
+  
+  constructor(private fbuilder: FormBuilder,
+    private flightsService: FlightsService) { }
+  
+  ngOnInit() {
+    this.form = this.fbuilder.group({
+      origin: '',
+      destination: '',
+      date: ''
+    });
+  
+  }
+  
+  sendToAPI() {
+    let formObj = this.form.getRawValue();
+  
+    let serializedForm = JSON.stringify(formObj);
+  
+    this.flightsService.search(serializedForm)
+      .then(flights => this.flights = flights);
+  }
 }
