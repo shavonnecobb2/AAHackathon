311f9c70ee49958bee23547be69a2909670e3da0
Zach Marbach <Zach.Marbach@improving.com>
2019-12-09 11:39:22 -0600

    added routing and modified flight object model

---
+++ front-end/src/app/flights/flights-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+
+const routes: Routes = [
+    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'checkout', component: CheckoutComponent},
+    {path: 'profile', component: ProfileComponent}
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- front-end/src/app/model.ts/aircraft.ts
+++
@@ -1,7 +0,0 @@
-export class Aircraft {
-    model: string;
-
-    constructor(aircraft: Aircraft){
-        this.model = aircraft.model;
-    }
-}
