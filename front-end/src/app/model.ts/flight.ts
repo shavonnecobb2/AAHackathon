bb0fbabea9d8a5b0fb82eb8487f4ddf7d362e50e
Taylor Cates <Taylor.Cates@improving.com>
2019-12-09 12:22:14 -0600

    Fixed flights.ts

--- front-end/src/app/model.ts/flight.ts
+++ front-end/src/app/model.ts/flight.ts
@@ -6,14 +6,15 @@ export class Flight {
     flightNum: string;
     origin: Origin;
     destination: Destination;
-    duration: Duration;
+    duration: Duration; 
+    aircraftModel: string;
     
     constructor(flight: Flight){
         this.flightNum = flight.flightNum;
         this.origin = flight.origin;
         this.destination = flight.destination;
         this.duration = flight.duration;
-
+        this.aircraftModel = flight.aircraftModel;
     }
 
 }
